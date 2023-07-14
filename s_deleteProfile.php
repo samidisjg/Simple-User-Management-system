9bd8b6c8d12f42b0a9adc1942833204efc7a5e9d
IT22607232Samidisjg <samidisjg@gmail.com>
2023-07-14 19:35:07 +0530

    final commit

---
+++ s_deleteProfile.php
@@ -0,0 +1,26 @@
+<?php
+include "connectionRamindu.php";
+session_start();
+$UserID = $_SESSION['uID'];
+
+
+$uName =$_SESSION['userName'];
+deleteData($uName);
+
+function deleteData($uName){
+    global $conn;
+
+    $sql = "DELETE FROM users WHERE userName = '" . $uName . "'";
+    
+    if($conn->query($sql) === TRUE) {
+        echo "Deleted Successfully";
+        header("Location:r_main.html");
+     
+        exit();
+    } else {
+        echo "Error: " . $conn->error;
+    }
+}
+
+$conn->close();
+?>
---
+++ s_js/s_delete.js
@@ -0,0 +1,15 @@
+ function confirmUpdate() {
+    var txt = confirm("Are you sure?Do You want to Update?");
+    if(!txt) {
+      return false;
+    }
+  }
+
+  function confirmDelete() {
+    var txt = confirm("Are you sure?Do You want to DELETE?");
+    if(!txt) {
+      return false;
+    }
+  }
+
+  
